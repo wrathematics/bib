00d0189e6814107b0c358a2b2446435146f364c2
Drew Schmidt <wrathematics@gmail.com>
2016-11-08 10:57:22 -0500

    add types

---
+++ src/types.h
@@ -0,0 +1,41 @@
+#ifndef RSVD_TYPES_H
+#define RSVD_TYPES_H
+
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef int len_t;
+
+// dense matrix
+typedef struct dmatrix_t
+{
+  len_t nrows;
+  len_t ncols;
+  double *data;
+} dmatrix_t;
+
+typedef struct dvector_t
+{
+  len_t len;
+  double *data;
+} dvector_t;
+
+typedef dmatrix_t *const restrict mat_r;
+typedef const dmatrix_t *const restrict cmat_r;
+
+typedef dvector_t *const restrict vec_r;
+typedef const dvector_t *const restrict cvec_r;
+
+
+
+typedef struct svd_t
+{
+  dvector_t *restrict d;
+  dmatrix_t *restrict u;
+  dmatrix_t *restrict vt;
+  int info;
+} svd_t;
+
+
+#endif
