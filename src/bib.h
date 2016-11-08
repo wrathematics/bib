c2807727c85a73a4615636bfb54976045f0fa161
Drew Schmidt <wrathematics@gmail.com>
2016-11-08 16:31:56 -0500

    add level2 blas to bib.h

--- src/bib.h
+++ src/bib.h
@@ -13,5 +13,27 @@ int setvec(clen_t len, double *data, dvector_t *v);
 dmatrix_t *newmat(clen_t nrows, clen_t ncols, double *data);
 int setmat(clen_t nrows, clen_t ncols, double *data, dmatrix_t *m);
 
+void freevec(dvector_t *v);
+void freemat(dmatrix_t *m);
+
+// level2.c
+#include <stdlib.h>
+
+#include "bib.h"
+#include "blas.h"
+#include "cdefs.h"
+
+
+// upper triangle of t(x) %*% x
+int crossprod(const double alpha, cmat_r x, mat_r cp);
+int crossprod_a(const double alpha, cmat_r x, mat_r cp);
+
+int tcrossprod(const double alpha, cmat_r x, mat_r tcp);
+int tcrossprod_a(const double alpha, cmat_r x, mat_r tcp);
+
+int mvprod(const bool trans, const double alpha, cmat_r x, cvec_r y, vec_r ret);
+int mvprod_a(const bool trans, const double alpha, cmat_r x, cvec_r y, vec_r ret);
+
+
 
 #endif
