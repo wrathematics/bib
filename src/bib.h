ab36c2b5017b633e0c8676d97ba94eee612bcb0e
Drew Schmidt <wrathematics@gmail.com>
2016-11-08 18:41:09 -0500

    add other prototypes to bib.h

--- src/bib.h
+++ src/bib.h
@@ -16,6 +16,13 @@ int setmat(clen_t nrows, clen_t ncols, double *data, dmatrix_t *m);
 void freevec(dvector_t *v);
 void freemat(dmatrix_t *m);
 
+// level1.c
+double dotprod(cvec_r x, cvec_r y);
+double vnorm(cvec_r x);
+int vswap(cvec_r x, vec_r y);
+int vcopy(cvec_r x, vec_r y);
+void vscale(const double alpha, vec_r x);
+
 // level2.c
 int crossprod(const double alpha, cmat_r x, mat_r cp);
 int crossprod_a(const double alpha, cmat_r x, mat_r cp);
@@ -26,6 +33,8 @@ int tcrossprod_a(const double alpha, cmat_r x, mat_r tcp);
 int mvprod(const bool trans, const double alpha, cmat_r x, cvec_r y, vec_r ret);
 int mvprod_a(const bool trans, const double alpha, cmat_r x, cvec_r y, vec_r ret);
 
+// level3.c
+int mmprod(const bool transx, const bool transy, cmat_r x, cmat_r y, mat_r ret);
 
 
 #endif
