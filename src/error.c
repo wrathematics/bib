318e3aa9e5861bcfdc3dd2f67675d7aec3f2a61d
Drew Schmidt <wrathematics@gmail.com>
2016-11-08 14:57:16 -0500

    error msg skeleton

---
+++ src/error.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "cdefs.h"
+
+void bib_errcheck(const int err)
+{
+  if (err == LIBBIB_OK)
+    return;
+  
+  fprintf(stderr, "ERROR: ");
+  
+  if (err == LIBBIB_BADMALLOC)
+    fprintf(stderr, "unable to allocate memory (malloc failed)");
+  else if (err == LIBBIB_INDIMMISMATCH)
+    fprintf(stderr, "input matrices/vectors are not conformable");
+  else if (err == LIBBIB_RETDIMMISMATCH)
+    fprintf(stderr, "return dimensions impossible for given input(s)");
+  else
+    fprintf(stderr, "unknown error code %d; please report this", err);
+  
+  fprintf(stderr, "\n");
+  exit(err);
+}
