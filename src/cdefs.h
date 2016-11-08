a8e304f2731bd93aa3cf2eb0e93eb60bde3ba814
Drew Schmidt <wrathematics@gmail.com>
2016-11-08 18:41:29 -0500

    more cdefs

--- src/cdefs.h
+++ src/cdefs.h
@@ -6,13 +6,14 @@
 #define MAX(a,b) (a<b?b:a)
 #define MIN(a,b) (a<b?a:b)
 
-#define LIBBIB_OK               0
-#define LIBBIB_BADMALLOC       -1
-#define LIBBIB_INDIMMISMATCH  -10
-#define LIBBIB_RETDIMMISMATCH -11
+#define LIBBIB_OK                0
+#define LIBBIB_BADMALLOC        -1
+#define LIBBIB_INDIMMISMATCH   -10
+#define LIBBIB_RETDIMMISMATCH  -11
+#define LIBBIB_RESTRICTFAIL   -100
 
 #define CHECKMALLOC(x) if (x == NULL) return LIBBIB_BADMALLOC
 #define CHECKRET(ret)  if (ret != 0)  return ret
-
+#define CHECKIFSAME(x,y) if(x==0) return LIBBIB_RESTRICTFAIL
 
 #endif
