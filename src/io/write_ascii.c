0ff2523bd75197e6157145c88840450a2bb70f1f
wrathematics <wrathematics@gmail.com>
2016-11-18 14:28:12 -0500

    add write fun

---
+++ src/io/write_ascii.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+#include "../cdefs.h"
+#include "../types.h"
+
+
+static int writemat_ascii_csv(FILE *fp, cmat_r x)
+{
+  const len_t nrows = NROWS(x);
+  const len_t ncols = NCOLS(x);
+  
+  for (len_t i=0; i<nrows; i++)
+  {
+    for (len_t j=0; j<ncols; j++)
+      fprintf(fp, "%23.14e,", DATA(x)[i + nrows*j]);
+    
+    fprintf(fp, "\n");
+  }
+  
+  return LIBBIB_OK;
+}
+
+
+
+static int writemat_ascii_arma(FILE *fp, cmat_r x)
+{
+  const len_t nrows = NROWS(x);
+  const len_t ncols = NCOLS(x);
+  
+  fprintf(fp, "ARMA_MAT_TXT_FN008\n");
+  fprintf(fp, "%d %d\n", nrows, ncols);
+  
+  for (len_t i=0; i<nrows; i++)
+  {
+    for (len_t j=0; j<ncols; j++)
+      fprintf(fp, "%23.14e", DATA(x)[i + nrows*j]);
+    
+    fprintf(fp, "\n");
+  }
+  
+  return LIBBIB_OK;
+}
+
+
+
+int bib_mwrite(const char *file, cmat_r x, const int method)
+{
+  int ret;
+  FILE *fp;
+  
+  fp = fopen(file, "w");
+  if (!fp)
+    return LIBBIB_WRITE_FAIL;
+  
+  if (method == LIBBIB_IO_ASCII_CSV)
+    ret = writemat_ascii_csv(fp, x);
+  else if (method == LIBBIB_IO_ASCII_ARMA)
+    ret = writemat_ascii_arma(fp, x);
+  else
+    ret = LIBBIB_IO_BADMETHOD;
+  
+  fclose(fp);
+  return ret;
+}
