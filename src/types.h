d6423c33eaed19337c76c25444d18bb30efaf207
wrathematics <wrathematics@gmail.com>
2016-11-18 14:33:14 -0500

    tweak types

--- src/types.h
+++ src/types.h
@@ -1,50 +1,50 @@
-#ifndef RSVD_TYPES_H
-#define RSVD_TYPES_H
+#ifndef LIBBIB_TYPES_H_
+#define LIBBIB_TYPES_H_
 
 
 #include <stdint.h>
 #include <stdbool.h>
 
-typedef int len_t;
+typedef int32_t len_t;
+typedef int64_t lenprod_t; // storage for len_t*len_t
 typedef const len_t clen_t;
 
 // dense matrix
-typedef struct dmatrix_t
+typedef struct dmatrix
 {
   len_t nrows;
   len_t ncols;
   double *data;
 } dmatrix_t;
 
-typedef struct dvector_t
+typedef struct dvector
 {
   len_t len;
   double *data;
 } dvector_t;
 
 typedef dmatrix_t *const restrict mat_r;
-typedef const dmatrix_t *const restrict cmat_r;
+typedef const mat_r cmat_r;
 
 typedef dvector_t *const restrict vec_r;
-typedef const dvector_t *const restrict cvec_r;
+typedef const vec_r cvec_r;
 
 
-
-typedef struct svd_t
-{
-  dvector_t *restrict d;
-  dmatrix_t *restrict u;
-  dmatrix_t *restrict vt;
-  int info;
-} svd_t;
-
-typedef struct svdparam_t
+typedef struct
 {
   bool inplace;
   bool retu;
   bool retvt;
   bool descending;
 } svdparam_t;
+
+typedef struct
+{
+  dvector_t *restrict d;
+  dmatrix_t *restrict u;
+  dmatrix_t *restrict vt;
+  int info;
+} svd_t;
 
 
 #endif
