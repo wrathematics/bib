a46058fbe1e34a1d0053319fc64ef843aeb976bb
wrathematics <wrathematics@gmail.com>
2016-11-19 15:50:08 -0500

    update error code handler

--- src/misc/error.c
+++ src/misc/error.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 #include "../cdefs.h"
 
@@ -10,7 +11,9 @@ void bib_errcheck(const int err)
   
   fprintf(stderr, "ERROR: ");
   
-  if (err == LIBBIB_BADMALLOC)
+  if (err == LIBBIB_OK)
+    return;
+  else if (err == LIBBIB_BADMALLOC)
     fprintf(stderr, "unable to allocate memory (malloc failed)");
   else if (err == LIBBIB_INDIMMISMATCH)
     fprintf(stderr, "input matrices/vectors are not conformable");
@@ -20,6 +23,21 @@ void bib_errcheck(const int err)
     fprintf(stderr, "input matrix is non-square");
   else if (err == LIBBIB_RESTRICTFAIL)
     fprintf(stderr, "inputs must be distinct");
+  else if (err == LIBBIB_LAPACKERR)
+    fprintf(stderr, "LAPACK error code %d", errno);
+  
+  // I/O
+  else if (err == LIBBIB_IO_BADMETHOD)
+    fprintf(stderr, "unknown I/O method");
+  else if (err == LIBBIB_READ_FAIL)
+    fprintf(stderr, "file read fail");
+  else if (err == LIBBIB_WRITE_FAIL)
+    fprintf(stderr, "file write fail");
+  else if (err == LIBBIB_READ_INVALID)
+    fprintf(stderr, "invalid in-file");
+  else if (err == LIBBIB_READ_BADVAL)
+    fprintf(stderr, "impossible value during file read");
+  
   else
     fprintf(stderr, "unknown error code %d; please report this", err);
   
