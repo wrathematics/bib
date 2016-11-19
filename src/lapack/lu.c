a31f3752801bd04d7af2f35d2198d529d0b5afbd
wrathematics <wrathematics@gmail.com>
2016-11-19 14:52:28 -0500

    add lu and det

--- src/lapack/lu.c
+++ src/lapack/lu.c
@@ -2,14 +2,68 @@
 #include <errno.h>
 
 #include "lapack.h"
+#include "../bib.h"
 #include "../cdefs.h"
 #include "../types.h"
 
 
-int invert(mat_r x)
+int bib_lu(mat_r x, ivec_r pivot)
 {
-  const int n = x->nrows;
-  if (n != x->ncols)
+  int ret = LIBBIB_OK;
+  int info;
+  const int m = NROWS(x);
+  const int n = NCOLS(x);
+  
+  dgetrf_(&m, &n, DATA(x), &m, DATA(pivot), &info);
+  if (info != 0)
+  {
+    errno = info;
+    ret = LIBBIB_LAPACKERR;
+  }
+  
+  return ret;
+}
+
+int bib_lu_a(mat_r x, ivector_t **pivot)
+{
+  const int m = NROWS(x);
+  const int n = NCOLS(x);
+  
+  *pivot = newivec(MIN(m, n));
+  if (DATA(*pivot) == NULL)
+    return LIBBIB_BADMALLOC;
+  
+  return bib_lu(x, *pivot);
+}
+
+
+
+int bib_det(mat_r x, double *det)
+{
+  int ret;
+  ivector_t *pivot;
+  const int n = NROWS(x);
+  if (n != NCOLS(x))
+    return LIBBIB_NONSQUARE;
+  
+  ret = bib_lu_a(x, &pivot);
+  CHECKRET(ret);
+  
+  *det = 1.0;
+  for (int i=0; i<n; i++)
+    *det *= DATA(x)[i + n*i];
+  
+  freeivec(pivot);
+  
+  return ret;
+}
+
+
+
+int bib_invert(mat_r x)
+{
+  const int n = NROWS(x);
+  if (n != NCOLS(x))
     return LIBBIB_NONSQUARE;
   
   double *const x_data = x->data;
